6b51ae7adb8506e38b49a4d076dd7847ee056dc3
Jannat <jannat@tirzok.com>
2019-10-30 17:39:47 +0600

    All files

---
+++ demo-docker/src/main/java/com/example/demodocker/DemoDockerApplication.java
@@ -0,0 +1,14 @@
+package com.example.demodocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class DemoDockerApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDockerApplication.class, args);
+	}
+
+}
